227a539ccae3344abf57269ca578e387d9b41ed2
Walter Arce <walterarce@gmail.com>
2021-09-19 18:15:04 -0300

    agrego sql para llamar en orden los store procedures

---
+++ Desarrollo/DDL/callproceduresenorden.sql
@@ -0,0 +1,9 @@
+call alta_cc('Marketing');
+call alta_cf('Buenos Aires');
+call alta_cliente('Walter','Arce' ,1,1);
+call alta_proyecto('Formulario de clientes',1,200);
+call alta_empleado('Geronimo', 'Perez',1220,25187617,'D');
+call alta_rol('Analista Full Stack');
+call asignar_rol_empleado(1220,1);
+call alta_tarea('Analisis implementacion React',1220,'A',1);
+call rendicion_horas_diaria(4.3,1)
